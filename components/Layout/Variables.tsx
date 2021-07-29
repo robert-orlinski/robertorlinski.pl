2801d8c55bdb802fe537000a06ce85d45e9cf2d3
Robert Orliński <kontakt@robertorlinski.pl>
2021-07-29 23:24:33 +0200

    fix CSS variables

--- components/Layout/Variables.tsx
+++ components/Layout/Variables.tsx
@@ -1,15 +1,16 @@
 import { createGlobalStyle } from 'styled-components';
 
-const Variables = createGlobalStyle`
-  --dark-gray: #222;
-  --nav-height: 96px;
+export const Variables = createGlobalStyle`
+  html {
+    --sans-serif: 'Darker Grotesque', -apple-system, sans-serif;
+    --serif: 'Playfair Display', serif;
 
-  --sans-serif: 'Darker Grotesque', -apple-system, sans-serif;
-  --serif: 'Playfair Display', serif;
+    --dark-gray: #222;
 
-  @media (max-width: 991px) {
-    --nav-height: 84px;
+    --nav-height: 96px;
+
+    @media (max-width: 991px) {
+      --nav-height: 84px;
+    }
   }
 `;
-
-export { Variables };
