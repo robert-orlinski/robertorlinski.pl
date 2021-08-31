7dd91274e4057dcb1aff3b239d8f360a173740c8
Robert Orliński <kontakt@robertorlinski.pl>
2021-08-31 08:45:09 +0200

    Add types for dynamic category slugs

--- types/data.d.ts
+++ types/data.d.ts
@@ -8,31 +8,19 @@ type Text = {
 
 export type ArrayOfTexts = Array<Text>;
 
-export type Category = {
-  name:
-    | 'Front-end'
-    | 'WordPress'
-    | 'Własne bloki w WordPress Gutenberg'
-    | 'Dostępność'
-    | 'Design'
-    | 'Narzędzia'
-    | 'Praca'
-    | 'Produktywność'
-    | 'Nauka'
-    | 'Przemyślenia';
-  slug:
-    | 'front-end'
-    | 'wordpress'
-    | 'wlasne-bloki-w-wordpress-gutenberg'
-    | 'dostepnosc'
-    | 'design'
-    | 'narzedzia'
-    | 'praca'
-    | 'produktywnosc'
-    | 'nauka'
-    | 'przemyslenia';
-};
+export type Category =
+  | 'Front-end'
+  | 'WordPress'
+  | 'Własne bloki w WordPress Gutenberg'
+  | 'Dostępność'
+  | 'Design'
+  | 'Narzędzia'
+  | 'Praca'
+  | 'Produktywność'
+  | 'Nauka'
+  | 'Przemyślenia';
 
 export type ListedCategory = {
+  name: Category;
   image: StaticImageData;
-} & Category;
+};
