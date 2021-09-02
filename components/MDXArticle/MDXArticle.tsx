ab33e936859d99a0c12ab3302955f3edae1474a2
Robert Orliński <kontakt@robertorlinski.pl>
2021-09-02 21:51:47 +0200

    Ok, P will be in MDXArticle but just as a styled-component

--- components/MDXArticle/MDXArticle.tsx
+++ components/MDXArticle/MDXArticle.tsx
@@ -1,10 +1,12 @@
 import { FC, useMemo } from 'react';
 import { getMDXComponent } from 'mdx-bundler/client';
 
+import { P } from 'components/P';
+
 type Props = { source: string };
 
 export const MDXArticle: FC<Props> = ({ source }) => {
   const MDXContainer = useMemo(() => getMDXComponent(source), [source]);
 
-  return <MDXContainer />;
+  return <MDXContainer components={{ p: P }} />;
 };
