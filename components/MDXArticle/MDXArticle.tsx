891dd605370b8da74e605984884ab4327856fb81
Robert Orliński <kontakt@robertorlinski.pl>
2021-09-02 21:37:31 +0200

    Restore P component to just styled-component version

--- components/MDXArticle/MDXArticle.tsx
+++ components/MDXArticle/MDXArticle.tsx
@@ -1,12 +1,10 @@
 import { FC, useMemo } from 'react';
 import { getMDXComponent } from 'mdx-bundler/client';
 
-import { P } from 'components/P';
-
 type Props = { source: string };
 
 export const MDXArticle: FC<Props> = ({ source }) => {
   const MDXContainer = useMemo(() => getMDXComponent(source), [source]);
 
-  return <MDXContainer components={{ p: P }} />;
+  return <MDXContainer />;
 };
--- components/P/P.tsx
+++ components/P/P.tsx
@@ -1,22 +1,11 @@
-import { FC } from 'react';
 import styled from 'styled-components';
 
 import { to } from 'Devices';
 
-export const StyledParagraph = styled.p`
+export const P = styled.p`
   padding-bottom: 1.5rem;
 
   @media ${to.phoneL} {
     padding-bottom: 1.2rem;
   }
 `;
-
-export const P: FC<any> = (props) => {
-  const { children } = props;
-
-  if (typeof children !== 'string' && children.type === 'img') {
-    return <>{children}</>;
-  }
-
-  return <StyledParagraph {...props} />;
-};
