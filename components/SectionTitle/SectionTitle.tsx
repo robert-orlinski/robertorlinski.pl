1b6d81a5137cf3b5ba2dc3a79266e19bb17e23de
Robert Orliński <kontakt@robertorlinski.pl>
2021-09-16 20:28:31 +0200

    Make section title a little bit smaller on laptops

--- components/SectionTitle/SectionTitle.tsx
+++ components/SectionTitle/SectionTitle.tsx
@@ -1,6 +1,12 @@
 import styled from 'styled-components';
 
+import { to } from 'Devices';
+
 export const SectionTitle = styled.h2`
   margin-bottom: 0.8em;
   font-size: clamp(1.5rem, 5vw, 4rem);
+
+  @media ${to.laptopL} {
+    font-size: clamp(1.5rem, 5vw, 3rem);
+  }
 `;
