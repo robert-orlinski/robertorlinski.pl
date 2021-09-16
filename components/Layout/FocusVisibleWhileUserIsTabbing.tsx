7b8f97d7f70fc881b6fcbff25d37d6a911c5c725
Robert Orliński <kontakt@robertorlinski.pl>
2021-09-16 17:21:10 +0200

    Add outline on interactive elements only if user is tabbing

---
+++ components/Layout/FocusVisibleWhileUserIsTabbing.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+export const FocusVisibleWhileUserIsTabbing = () => {
+  const [userIsTabbing, setUserTabbing] = useState(false);
+
+  useEffect(() => {
+    const handleFirstTab = (e: KeyboardEvent) => {
+      if (e.key === 'Tab') {
+        setUserTabbing(true);
+      }
+    };
+
+    const handleMouseDown = () => {
+      setUserTabbing(false);
+    };
+
+    const addListeners = () => {
+      window.addEventListener('keydown', handleFirstTab);
+      window.addEventListener('mousedown', handleMouseDown);
+    };
+
+    const removeListeners = () => {
+      window.removeEventListener('keydown', handleFirstTab);
+      window.removeEventListener('mousedown', handleMouseDown);
+    };
+
+    addListeners();
+
+    return () => removeListeners();
+  }, [userIsTabbing]);
+
+  if (!userIsTabbing) return <FocusInvisible />;
+
+  return <FocusVisible />;
+};
+
+const FocusInvisible = createGlobalStyle`
+  :focus {
+    outline: none;
+  }
+`;
+
+const FocusVisible = createGlobalStyle`
+  :focus {
+    outline: 2px solid #7AACFE;
+  }
+`;
--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 
 import { CssVariables } from './CssVariables';
 import { GlobalStyle } from './GlobalStyle';
+import { FocusVisibleWhileUserIsTabbing } from './FocusVisibleWhileUserIsTabbing';
 
 import { Footer } from '../Footer';
 
@@ -9,6 +10,7 @@ export const Layout: FC = ({ children }) => (
   <>
     <CssVariables />
     <GlobalStyle />
+    <FocusVisibleWhileUserIsTabbing />
     {children}
     <Footer />
   </>
