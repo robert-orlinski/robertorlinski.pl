6ad0533cc8ec6bacb97c85c0bdd09781db2a420a
Robert Orliński <kontakt@robertorlinski.pl>
2021-09-25 13:54:07 +0200

    Add 2 redirects

--- next.config.js
+++ next.config.js
@@ -1,3 +1,24 @@
+const transferRedirects = [
+  {
+    source: '/wszystkie-wpisy',
+    destination: '/artykuly',
+    permanent: true,
+  },
+];
+
+const postRedirects = [
+  {
+    source: '/stworzyc-motyw-wordpress-001-instalacja-serwera-lokalnego',
+    destination: '/artykuly/jak-stworzyc-motyw-wordpress-001',
+    permanent: true,
+  },
+];
+
+const redirects = transferRedirects.concat(postRedirects);
+
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return redirects;
+  },
 };
