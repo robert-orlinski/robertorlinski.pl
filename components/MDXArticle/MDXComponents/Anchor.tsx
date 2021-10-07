b67d76bc6adc16f10a797a56f9e2ccd6f7df49f8
Robert Orliński <kontakt@robertorlinski.pl>
2021-10-07 21:38:28 +0200

    Fix Anchor component to link images also to external destinations

--- components/MDXArticle/MDXComponents/Anchor.tsx
+++ components/MDXArticle/MDXComponents/Anchor.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 import { Link } from 'Components/Link';
 
 export const Anchor: FC<any> = ({ children, href }) => {
-  if (children.type === 'img') {
+  if (children.type === 'img' && !href) {
     return (
       <Link href={children.props.src} isExternal>
         {children}
