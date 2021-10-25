8a67f7013935e3c787fe3360bd16a4f7e7206862
Robert Orliński <kontakt@robertorlinski.pl>
2021-10-25 20:39:31 +0200

    Change e-mail input pattern

--- components/NewsletterForm/EmailInput.tsx
+++ components/NewsletterForm/EmailInput.tsx
@@ -3,14 +3,14 @@ import { FieldValues } from 'react-hook-form';
 
 import { TextInput } from 'Components/TextInput';
 
-import { DATE_PATTERN } from 'Helpers/constants';
+import { EMAIL_PATTERN } from 'Helpers/constants';
 
 export const EmailInput: FC<FieldValues> = ({ register, errors }) => (
   <TextInput
     label="Twój e-mail"
     name="email"
     required={true}
-    pattern={DATE_PATTERN}
+    pattern={EMAIL_PATTERN}
     autoComplete="email"
     {...{ register, errors }}
   />
