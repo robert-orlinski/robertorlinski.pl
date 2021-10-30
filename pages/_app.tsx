a50f11f27d9c9932914ea00efa0dc56b6f0be148
Robert Orliński <kontakt@robertorlinski.pl>
2021-10-30 21:51:18 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,10 @@ import { AppProps } from 'next/app';
 
 import { Layout } from 'Components/Layout';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
-}
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <Layout>
+    <Component {...pageProps} />
+  </Layout>
+);
 
 export default MyApp;
