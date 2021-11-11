8763a4f9e22ee837588b321b7f6b6aa1698c4a93
Robert Orliński <kontakt@robertorlinski.pl>
2021-11-11 13:58:38 +0100

    Remove DATE_PATTERN from constants

--- helpers/constants.ts
+++ helpers/constants.ts
@@ -1,7 +1,5 @@
 import path from 'path';
 
-export const DATE_PATTERN = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
-
 export const EMAIL_PATTERN = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
 
 export const POSTS_PATH = path.join(process.cwd(), 'content/posts');
