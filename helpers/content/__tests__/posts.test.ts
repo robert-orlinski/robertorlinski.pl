a0fa5e839ac11ff769d0e3d115a7b48152b80c95
Robert Orliński <kontakt@robertorlinski.pl>
2021-11-23 10:08:07 +0100

    Add posts tests

---
+++ helpers/content/__tests__/posts.test.ts
@@ -0,0 +1,133 @@
+import fs from 'fs';
+import dayjs from 'dayjs';
+
+import {
+  getPosts,
+  getPostsByDateDescending,
+  getPostBySlug,
+  getPopularPosts,
+  getRelatedPosts,
+} from '../posts';
+
+jest.mock('../../constants', () => {
+  const originalModule = jest.requireActual('../../constants') as any;
+  const mockedPath = originalModule.POSTS_PATH.replace('content', '__mocks__');
+
+  return {
+    ...originalModule,
+    POSTS_PATH: mockedPath,
+  };
+});
+
+jest.spyOn(require('../mdx'), 'default').mockImplementation((source, { resourcePath }: any) => {
+  const dirForFeaturedImage = resourcePath.replace('__mocks__', 'public/images');
+
+  fs.access(dirForFeaturedImage, function (err) {
+    if (err && err.code === 'ENOENT') {
+      fs.mkdirSync(dirForFeaturedImage);
+    }
+  });
+
+  const mockedResourceContent = fs.readFileSync(`${resourcePath}/index.json`, 'utf8');
+
+  return JSON.parse(mockedResourceContent);
+});
+
+describe('sorting posts', () => {
+  it('sorts posts by date', async () => {
+    const posts = await getPosts();
+
+    const sortedPosts = posts.sort((a, b) => {
+      const firstDate = dayjs(a.date, 'DD.MM.YYYY');
+      const secondDate = dayjs(b.date, 'DD.MM.YYYY');
+
+      return firstDate.isBefore(secondDate) ? 1 : -1;
+    });
+
+    expect(sortedPosts).toEqual(posts);
+  });
+
+  it('sorts posts by date and uses sorting function', async () => {
+    const posts = await getPosts();
+    const sortedPosts = await getPostsByDateDescending();
+
+    expect(sortedPosts).toEqual(posts);
+  });
+});
+
+describe('retrieving posts list', () => {
+  it('returns 5 posts from folder which contains 5 posts', async () => {
+    const posts = await getPosts();
+
+    expect(posts).toHaveLength(5);
+  });
+
+  it('contains "metodologia-bem" post\'s title', async () => {
+    const posts = await getPosts();
+    const postTitles = posts.map(({ title }) => title);
+
+    expect(postTitles).toContain('Metodologia BEM – wszystko, co trzeba o niej wiedzieć');
+  });
+
+  it('returns all popular posts', async () => {
+    const popularPosts = await getPopularPosts();
+
+    expect(popularPosts).toHaveLength(3);
+  });
+});
+
+describe('single post', () => {
+  it('returns post by its slug', async () => {
+    const post = await getPostBySlug('with-code');
+
+    expect(post).toBeDefined();
+  });
+
+  it('returns post that contains proper title', async () => {
+    const post = await getPostBySlug('with-code');
+
+    expect(post.metaData.title).toContain(
+      '13 dobrych praktyk, które sprawią, że Twój kod CSS stanie się lepszy!',
+    );
+  });
+
+  it('returns post that contains proper content', async () => {
+    const post = await getPostBySlug('with-code');
+
+    expect(post.content).toContain('Korzystanie z dobrych praktyk ma szereg zalet.');
+  });
+});
+
+describe('related posts', () => {
+  it('returns 3 related posts', async () => {
+    const relatedPosts = await getRelatedPosts('with-code', 'Front-end');
+
+    expect(relatedPosts).toHaveLength(3);
+  });
+
+  it('not return current post', async () => {
+    const relatedPosts = await getRelatedPosts('with-code', 'Front-end');
+
+    relatedPosts.forEach(({ slug }) => {
+      expect(slug).not.toEqual('with-code');
+    });
+  });
+
+  it('returns 2 posts from current topic', async () => {
+    const relatedPosts = await getRelatedPosts('with-code', 'Front-end');
+
+    const relatedPostsFromCurrentTopic = relatedPosts.filter(({ topics }) =>
+      topics.includes('Front-end'),
+    );
+
+    expect(relatedPostsFromCurrentTopic.length).toEqual(2);
+  });
+
+  it('returns 1 newest post from different than current topic', async () => {
+    const relatedPosts = await getRelatedPosts('with-code', 'Front-end');
+
+    const postsTopics = relatedPosts.map(({ topics }) => topics).flat();
+
+    expect(postsTopics[2]).toContain('WordPress');
+  });
+});
