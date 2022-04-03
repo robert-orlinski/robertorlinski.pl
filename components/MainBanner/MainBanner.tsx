194d2a2403aa682dd92cf5b79d5843ff2a106d88
Robert Orliński <kontakt@robertorlinski.pl>
2022-04-03 20:25:00 +0200

    Disable MainBanner image lazy loading

--- components/MainBanner/MainBanner.tsx
+++ components/MainBanner/MainBanner.tsx
@@ -28,11 +28,18 @@ const MainBanner: FC<Props> = ({ title, image: { src, alt }, isImageFluid }) => 
         <MainTitle>{title}</MainTitle>
         {isImageFixed ? (
           <FixedImageContainer>
-            <Image {...{ src, alt }} width="880" height="560" objectFit="cover" priority />
+            <Image
+              {...{ src, alt }}
+              width="880"
+              height="560"
+              objectFit="cover"
+              priority
+              loading="eager"
+            />
           </FixedImageContainer>
         ) : (
           <ImageContainer>
-            <Image {...{ src, alt }} priority />
+            <Image {...{ src, alt }} priority loading="eager" />
           </ImageContainer>
         )}
       </Banner>
