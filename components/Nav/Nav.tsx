a4f046b5bc484d9ef2cb1bf83cd7053fbd504846
unknown <mr.pokorski@gmail.com>
2022-06-26 16:26:33 +0200

    Change handling functionality of closing main menu on click away
    Mainly changes which reflect changes in the project - instead of using custom hook, used ClickAwayListener component.

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,9 +1,7 @@
 import styled from 'styled-components';
-import { useRef } from 'react';
 
 import useToggle from 'Hooks/useToggle';
 import useStickyElement from 'Hooks/useStickyElement';
-import useOnClickOutside from 'Hooks/useOnClickOutside';
 
 import HiddenMainPageTitle from 'Components/HiddenMainPageTitle';
 import Separator from 'Components/Separator';
@@ -11,6 +9,7 @@ import SocialMedia from 'Components/SocialMedia';
 import Search from 'Components/Search';
 import Hamburger from 'Components/Hamburger';
 import Brand from 'Components/Brand';
+import ClickAwayListener from 'Components/ClickAwayListener';
 
 import NavBar from './parts/NavBar';
 import NavMenu from './parts/NavMenu';
@@ -22,33 +21,32 @@ const Nav = () => {
   const [isNavVisibleOnMobile, toggleNavVisibility] = useToggle(false);
   const [isSticky] = useStickyElement();
 
-  const container = useRef<HTMLDivElement>(null);
-  useOnClickOutside(container, () => {
-    if (isNavVisibleOnMobile) {
-      toggleNavVisibility();
-    }
-  });
+  const handleClickAway = () => {
+    if (isNavVisibleOnMobile) toggleNavVisibility();
+  };
 
   return (
     <NavBar {...{ isSticky }}>
       <HiddenMainPageTitle />
       <Brand />
-      <Container ref={container}>
-        <NavLinks areVisible={isNavVisibleOnMobile}>
-          <NavMenu />
-          <SocialMedia />
-        </NavLinks>
-        <SeparatorHiddenOnMobile />
-        <Container>
-          <Search />
-          <Hamburger
-            onClick={toggleNavVisibility}
-            isCrossed={isNavVisibleOnMobile}
-            ariaLabelForClosed="Otwórz menu nawigacyjne"
-            ariaLabelForOpened="Zamknij menu nawigacyjne"
-            isHiddenOnDesktop={true}
-          />
-        </Container>
+      <Container>
+        <ClickAwayListener onClickAway={handleClickAway}>
+          <NavLinks areVisible={isNavVisibleOnMobile}>
+            <NavMenu />
+            <SocialMedia />
+          </NavLinks>
+          <SeparatorHiddenOnMobile />
+          <Container>
+            <Search />
+            <Hamburger
+              onClick={toggleNavVisibility}
+              isCrossed={isNavVisibleOnMobile}
+              ariaLabelForClosed="Otwórz menu nawigacyjne"
+              ariaLabelForOpened="Zamknij menu nawigacyjne"
+              isHiddenOnDesktop={true}
+            />
+          </Container>
+        </ClickAwayListener>
       </Container>
     </NavBar>
   );
