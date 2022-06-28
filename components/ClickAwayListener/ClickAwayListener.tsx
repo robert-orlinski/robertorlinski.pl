8f08013b67b8ca6b0d971541175de970a0ef7493
unknown <mr.pokorski@gmail.com>
2022-06-28 02:16:41 +0200

    Delegate click away event detection from ClickAwayListener
    Moved handling of detecting click away event from ClickAwayListener component to the newly created custom hook useOnClickAway.

--- components/ClickAwayListener/ClickAwayListener.tsx
+++ components/ClickAwayListener/ClickAwayListener.tsx
@@ -1,4 +1,5 @@
-import React, { FC, useEffect, useRef } from 'react';
+import { FC, useRef } from 'react';
+import useOnClickAway from 'Hooks/useOnClickAway';
 
 type Props = {
   onClickAway: () => void;
@@ -6,21 +7,7 @@ type Props = {
 
 const ClickAwayListener: FC<Props> = ({ children, onClickAway }) => {
   const containerRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const handleAction = (event: MouseEvent | TouchEvent) => {
-      if (!containerRef.current || containerRef.current.contains(event.target as Element)) return;
-      onClickAway();
-    };
-
-    document.addEventListener('mousedown', handleAction);
-    document.addEventListener('touchstart', handleAction);
-
-    return () => {
-      document.removeEventListener('mousedown', handleAction);
-      document.removeEventListener('touchstart', handleAction);
-    };
-  }, [containerRef, onClickAway]);
+  useOnClickAway(containerRef, () => onClickAway());
 
   return <div ref={containerRef}>{children}</div>;
 };
