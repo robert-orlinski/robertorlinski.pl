ca9b1c2346886ce9c0841a78da37eca0a95ac3a8
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-07-17 01:48:52 +0200

    Fix bug in getISOStringFromPublicationDate.ts
    Subtract 1 from month index in new Date instance

--- helpers/functions/getISOStringFromPublicationDate.ts
+++ helpers/functions/getISOStringFromPublicationDate.ts
@@ -3,7 +3,7 @@ const getISOStringFromPublicationDate = (date: string): string => {
 
   return new Date(
     Number(splittedDate[2]), // Year
-    Number(splittedDate[1]), // Month
+    Number(splittedDate[1]) - 1, // Month
     Number(splittedDate[0]), // Day
   ).toISOString();
 };
