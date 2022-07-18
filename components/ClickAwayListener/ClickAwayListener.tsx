8187df1feaba6d0eeacc19debdf50e78b0b19df8
Grzegorz Pokorski <mr.pokorski@gmail.com>
2022-07-18 12:20:49 +0200

    Replace React.Fragment with shorthand tag

--- components/ClickAwayListener/ClickAwayListener.tsx
+++ components/ClickAwayListener/ClickAwayListener.tsx
@@ -11,7 +11,7 @@ const ClickAwayListener: FC<Props> = ({ children, onClickAway }) => {
 
   if (React.Children.count(children) == 1) {
     const copyWithRef = React.cloneElement(children as ReactElement, { ref: ref });
-    return <React.Fragment>{copyWithRef}</React.Fragment>;
+    return <>{copyWithRef}</>;
   }
 
   return <div ref={ref as RefObject<HTMLDivElement>}>{children}</div>;
