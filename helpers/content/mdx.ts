a7c52a851a5ccd48e5ae1c93d8620db26dafb3bc
Robert Orliński <kontakt@robertorlinski.pl>
2022-10-02 19:30:44 +0200

    Remove code overwriting process.env.ESBUILD_BINARY_PATH

--- helpers/content/mdx.ts
+++ helpers/content/mdx.ts
@@ -17,23 +17,6 @@ const prepareMDX = async (
     [key: string]: any;
   }>
 > => {
-  if (process.platform === 'win32') {
-    process.env.ESBUILD_BINARY_PATH = path.join(
-      process.cwd(),
-      'node_modules',
-      'esbuild',
-      'esbuild.exe',
-    );
-  } else {
-    process.env.ESBUILD_BINARY_PATH = path.join(
-      process.cwd(),
-      'node_modules',
-      'esbuild',
-      'bin',
-      'esbuild',
-    );
-  }
-
   const { resourcePath, imagesDirectory } = options;
 
   const { code, frontmatter } = await bundleMDX({
